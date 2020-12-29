dac121d56798a1f69b6bb6497b4a07f0fa180f2f
dilan891 <dilandeornelas@gmail.com>
2020-12-28 22:25:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kvvxym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kvvxym)
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <ul className="navbar">
+          <li>Incio</li>
+          <li>Mercado</li>
+          <li>Memes</li>
+          <li>Soporte</li>
+          <li>Alimento</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
